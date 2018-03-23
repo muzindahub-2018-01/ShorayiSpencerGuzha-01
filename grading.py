86c52275dd711df7be374125ea2e837c9b4c500a
ShorayiSpencer <37509073+ShorayiSpencer@users.noreply.github.com>
2018-03-23 14:07:13 +0200

    Add files via upload
    This is a simple grading system that grades marks between 0 and 100 with a range from A+ to fail.

---
+++ grading.py
@@ -0,0 +1,26 @@
+# This is a mark grading system 
+
+#This is the mark input
+mark=int(input(' Please Enter Your Mark: \n'))
+
+#this is the mark range of the grades and the corresponding grade
+if mark<=100 and mark>=80:
+    print('Excellent You got A+ grade!')
+elif mark<79 and mark>75:
+    print('You got A grade')
+elif mark < 75 and mark > 70:
+    print('You got A- grade')
+elif mark < 70 and mark > 65:
+    print('You got B+ grade')
+elif mark < 65 and mark > 60:
+    print('You got B grade')
+elif mark <60 and mark >50:
+    print('You got C+ grade')
+elif mark <50 and mark >40:
+    print('You got C grade')
+elif mark<0:
+    print('Invalid Option!')
+
+else :
+    print('Sorry! You are failed')
+
